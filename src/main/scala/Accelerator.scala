dbd4ddb0398119ee806278f0acceed687a3375d8
shuokai <kkm981104@gmail.com>
2022-11-30 13:01:21 +0100

    Updated Accelerator.scala

--- src/main/scala/Accelerator.scala
+++ src/main/scala/Accelerator.scala
@@ -14,13 +14,14 @@ class Accelerator extends Module {
   })
 
   // State enum and register
-  val idle :: xLoop :: yLoop :: checkBorder :: isBlackPixel :: writeBlack :: checkLeft :: checkRight :: checkUp :: checkDown :: writeWhite :: yInc :: xInc :: done :: Nil = Enum(14)
+  val idle :: xLoop :: yLoop :: checkBorder :: isBlackPixel :: writeBlack :: writeNextBlack :: checkLeft :: checkRight :: checkUp :: checkDown :: yInc :: xInc :: done :: Nil = Enum(14)
   val stateReg = RegInit(idle)
 
   // Support registers
   val xReg = RegInit(0.U(16.W))
   val yReg = RegInit(0.U(16.W))
   val inReg = RegInit(0.U(16.W))
+  val dataRead = RegInit(0.U(32.W))
 
   // Default values
   io.done := false.B
@@ -43,6 +44,7 @@ class Accelerator extends Module {
       }
     }
     is (yLoop) {
+      io.writeEnable := false.B
       when (yReg <= 19.U) {
         inReg := xReg + (20.U * yReg)
         stateReg := checkBorder
@@ -51,16 +53,20 @@ class Accelerator extends Module {
       }
     }
     is (checkBorder) {
+      io.dataWrite := 0.U
       when (xReg === 0.U || xReg === 19.U || yReg === 0.U || yReg === 19.U) {
         stateReg := writeBlack
       } .otherwise {
+        io.address := inReg
+        dataRead := io.dataRead
         stateReg := isBlackPixel
       }
     }
     is (isBlackPixel) {
-      io.address := inReg
-      when (io.dataRead === 0.U) {
-        stateReg := writeBlack
+      when (dataRead === 0.U) {
+        io.address := inReg + 400.U
+        io.writeEnable := true.B
+        stateReg := writeNextBlack
       } .otherwise {
         io.address := inReg + 20.U // Get pixel below
         dataRead := io.dataRead
@@ -68,14 +74,13 @@ class Accelerator extends Module {
       }
     }
     is (writeNextBlack) {
-      io.address := inReg + 400.U + 20.U
+      io.address := inReg + 400.U + 1.U
       yReg := yReg + 2.U
       stateReg := yLoop
     }
     is (writeBlack) {
       io.address := inReg + 400.U
       io.writeEnable := true.B
-      io.dataWrite := 0.U
       stateReg := yInc
     }
     is(checkDown) {
@@ -84,7 +89,7 @@ class Accelerator extends Module {
         io.writeEnable := true.B
         stateReg := writeNextBlack
       }.otherwise {
-        io.address := inReg - 1.U // Get pixel to the left
+        io.address := inReg - 1.U
         dataRead := io.dataRead
         stateReg := checkLeft
       }
@@ -93,31 +98,31 @@ class Accelerator extends Module {
       when (dataRead === 0.U) {
         stateReg := writeBlack
       } .otherwise {
-        io.address := inReg + 1.U // Get pixel to the right
+        io.address := inReg + 1.U // Get pixel to the left
         dataRead := io.dataRead
         stateReg := checkRight
       }
     }
     is (checkRight) {
-      io.address := inReg + 1.U // Get pixel to the right
-      when (io.dataRead === 0.U) {
+      when (dataRead === 0.U) {
         stateReg := writeBlack
       } .otherwise {
-        io.address := inReg - 20.U // Get pixel above
+        io.address := inReg - 20.U // Get pixel to the left
         dataRead := io.dataRead
         stateReg := checkUp
       }
     }
     is (checkUp) {
-      io.address := inReg - 20.U // Get pixel above
-      when (io.dataRead === 0.U) {
+      when (dataRead === 0.U) {
         stateReg := writeBlack
       } .otherwise {
-        stateReg := checkDown
+        io.writeEnable := true.B
+        io.dataWrite := 255.U
+        io.address := inReg + 400.U // Output address
+        stateReg := yInc
       }
     }
     is (yInc) {
-      io.writeEnable := false.B
       yReg := yReg + 1.U
       stateReg := yLoop
     }
